4523ab34ec29ee1580bb55742d70d333e482ca0c
huining999 <57716942+huining999@users.noreply.github.com>
2019-11-13 22:20:06 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+     项目名称：第一个Java程序
+     项目功能：打印一句话
+     作    者：小马哥
+     版    本：v1.0
+     所 有 者：兄弟连IT培训
+     备    注：困了的同学请主动站起来！！！
+ */
+public class HelloWorld/*类名*/ {/*类体*/
+
+    public static void main/*主方法名是程序的入口*/(String[] args) {/*方法体*/
+
+        System.out.println("我就不打印HelloWorld！");
+    }
+}
